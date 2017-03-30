9b582785b816b7aa7a2e88bdfde5dd7771e6f375
rmiller34 <rosemiller34@gmail.com>
2017-03-29 21:28:30 -0500

    Add files via upload

---
+++ level_leg_timesteps_0914.m
@@ -0,0 +1,9 @@
+% ------Level_leg_timesteps_0914-----%
+
+new_summary_start=3;
+
+level_leg_1_cas=5545-new_summary_start:6225-new_summary_start;
+level_leg_2_cas=10470-new_summary_start:10900-new_summary_start;
+level_leg_3_cas=16020-new_summary_start:16537-new_summary_start;
+level_leg_4_cas=17560-new_summary_start:18082-new_summary_start;
+level_leg_5_cas=22670-new_summary_start:22846-new_summary_start;
---
+++ sawtooth_timesteps_0914.m
@@ -0,0 +1,13 @@
+%-----sawtooth_timesteps_0914-----%
+
+new_summary_start_cas=3;
+
+
+sawtooth_1_cas=395-new_summary_start_cas:460-new_summary_start_cas;
+sawtooth_2_cas=6210-new_summary_start_cas:6265-new_summary_start_cas;
+sawtooth_3_cas=6779-new_summary_start_cas:6863-new_summary_start_cas;
+sawtooth_4_cas=10324-new_summary_start_cas:10470-new_summary_start_cas;
+sawtooth_5_cas=11790-new_summary_start_cas:11850-new_summary_start_cas;
+sawtooth_6_cas=17410-new_summary_start_cas:17575-new_summary_start_cas;
+sawtooth_7_cas=18125-new_summary_start_cas:18189-new_summary_start_cas;
+sawtooth_8_cas=18525-new_summary_start_cas:18589-new_summary_start_cas;
